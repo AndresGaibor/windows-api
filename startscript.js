218bbceb8444f08f54b64cf39df1f33c6da447c5
Andres Gaibor <androymartin99@gmail.com>
2023-08-29 15:46:47 -0500

    start 3

--- startscript.js
+++ startscript.js
@@ -1,15 +1,12 @@
 const exec = require("child_process").exec;
 
-exec(
-  "npm install && npm start",
-  { windowsHide: true },
-  (err, stdout, stderr) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log(stdout);
-    console.log(stderr);
-    console.log("Starting server...");
+exec("npm install", { windowsHide: false }, (err, stdout, stderr) => {
+  if (err) {
+    console.log(err);
+    return;
   }
-);
+  console.log(stdout);
+  console.log(stderr);
+  console.log("Starting server...");
+  exec("npm start", { windowsHide: false });
+});
