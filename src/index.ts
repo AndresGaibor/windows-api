53a8420a132077ff91a5f6f946f7bdb64e545bd5
Andres Gaibor <androymartin99@gmail.com>
2023-08-29 10:59:51 -0500

    prueba nutjs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello World");
+import { clipboard } from "@nut-tree/nut-js";
+
+(async () => {
+  await clipboard.setContent("Hello World!");
+
+  const clipboardText = await clipboard.getContent();
+})();
