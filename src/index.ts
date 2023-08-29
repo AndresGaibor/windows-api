b066e59e63294b8243ab0c3a30ed9345cb61d1a6
Andres Gaibor <androymartin99@gmail.com>
2023-08-29 14:23:36 -0500

    prueba api 3

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,7 @@ const fastify = Fastify({
 
 // Declare a route
 fastify.get("/", async function handler(request, reply) {
-  return { hello: "mundo 2" };
+  return { hello: "mundo 3" };
 });
 fastify.post("/git", async function handler(request, reply) {
   const cmd = require("node-cmd");
