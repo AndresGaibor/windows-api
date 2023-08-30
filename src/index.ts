abae039b6128a06ebbb507c28e0001a76fdca271
Andres Gaibor <androymartin99@gmail.com>
2023-08-29 22:51:07 -0500

    arreglo de sleep

--- src/index.ts
+++ src/index.ts
@@ -60,7 +60,7 @@ app.get('/resources/:name', async (request, reply) => {
 })
 
 app.get('/position', async (request, reply) => {
-  sleep(5000)
+  await sleep(5000)
   const position = await mouse.getPosition()
   return position
 })
