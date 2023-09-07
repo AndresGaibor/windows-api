0c6ba17c2da8694f6dee6571372b94e33a759f45
Andres Gaibor <androymartin99@gmail.com>
2023-09-07 13:44:48 -0500

    errores de ruta

--- src/controllers/screenshotController.ts
+++ src/controllers/screenshotController.ts
@@ -2,26 +2,26 @@ import fs from 'fs'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { screen, Region } from '@nut-tree/nut-js'
 import path from 'path'
-import { directorioRaiz } from '..'
+import { directorioRaiz } from '../constants'
 
 export async function screenshotHandler(
-  request: FastifyRequest,
-  reply: FastifyReply
+   request: FastifyRequest,
+   reply: FastifyReply
 ) {
-  // return a screenshot
-  const width = await screen.width()
-  const height = await screen.height()
+   // return a screenshot
+   const width = await screen.width()
+   const height = await screen.height()
 
-  const halfWidth = width / 2
+   const halfWidth = width / 2
 
-  await screen.highlight(new Region(halfWidth, 0, halfWidth, height))
-  await screen.capture('screenshot.png')
-  const rutaArchivo = path.join(directorioRaiz, 'screenshot.png')
-  // return schreenshot.png
-  const bufferIndexHtml = fs.readFileSync(rutaArchivo)
-  // delete file
-  fs.unlinkSync(rutaArchivo)
-  reply.type('image/png').send(bufferIndexHtml)
+   await screen.highlight(new Region(halfWidth, 0, halfWidth, height))
+   await screen.capture('screenshot.png')
+   const rutaArchivo = path.join(directorioRaiz, 'screenshot.png')
+   // return schreenshot.png
+   const bufferIndexHtml = fs.readFileSync(rutaArchivo)
+   // delete file
+   fs.unlinkSync(rutaArchivo)
+   reply.type('image/png').send(bufferIndexHtml)
 }
 
 export default screenshotHandler
--- src/models/factura.ts
+++ src/models/factura.ts
@@ -1,45 +1,3 @@
-export class Establecimiento {
-   /**
-    *
-    * @param codigo Codigo del establecimiento para la secuencia de facturas - ejemplo: 1
-    * @param nombre Nombre del establecimiento - ejemplo: Cuenca
-    */
-   constructor(
-      private _id: number,
-      private _codigo: number,
-      private _nombre: string
-   ) {}
-
-   get codigo(): number {
-      return this._codigo
-   }
-
-   get nombre(): string {
-      return this._nombre
-   }
-}
-
-export class PuntoEmision {
-   constructor(
-      private _id: number,
-      private _codigo: number,
-      private _nombre: string,
-      private _establecimiento: Establecimiento
-   ) {}
-
-   get codigo(): number {
-      return this._codigo
-   }
-
-   get nombre(): string {
-      return this._nombre
-   }
-
-   get establecimiento(): Establecimiento {
-      return this._establecimiento
-   }
-}
-
 /**
  *
  * @param text Texto a formatear - ejemplo: 1
@@ -61,37 +19,6 @@ function formatString(
    return textString
 }
 
-export class Secuencial {
-   #puntoEmision: PuntoEmision
-   #numero: number
-
-   constructor(puntoEmision: PuntoEmision, numero: number) {
-      this.#puntoEmision = puntoEmision
-      this.#numero = numero
-   }
-
-   get puntoEmision(): PuntoEmision {
-      return this.#puntoEmision
-   }
-
-   get numero(): number {
-      return this.#numero
-   }
-
-   incrementar(): void {
-      this.#numero++
-   }
-
-   toPrint(): string {
-      const establecimientoCodigo = formatString(
-         this.#puntoEmision.establecimiento.codigo,
-         3
-      )
-      const puntoEmisionCodigo = formatString(this.#puntoEmision.codigo, 3)
-      const numero = formatString(this.#numero, 9)
-      return `${establecimientoCodigo}-${puntoEmisionCodigo}-${numero}`
-   }
-}
 export interface Factura {
    establecimiento: number
    puntoEmision: number
@@ -107,14 +34,4 @@ export interface FacturaCreada extends Factura {
    id: number
 }
 
-class Database {}
-
 export default Factura
-
-const establecimiento = new Establecimiento(1, 1, 'Cuenca')
-const puntoEmision = new PuntoEmision(1, 1, 'Principal', establecimiento)
-const secuencia = new Secuencial(puntoEmision, 1)
-secuencia.incrementar()
-secuencia.puntoEmision.establecimiento.nombre
-
-const factura = new Factura(secuencia, 1, new Date())
