34ef75e38616f56154d6d280d22fa2ae75a9e89f
Andres Gaibor <androymartin99@gmail.com>
2023-09-10 17:03:42 -0500

    startscript

--- startscript.js
+++ startscript.js
@@ -1,20 +1,28 @@
-const util = require("util");
-const exec = util.promisify(require("child_process").exec);
+const util = require('util')
+const { execSync } = require('child_process')
+const exec = util.promisify(require('child_process').exec)
 
-async function run() {
-  try {
-    console.log("Installing dependencies...");
-    const installResult = await exec("npm install");
-    console.log(installResult.stdout);
-    console.log(installResult.stderr);
+// first two args can be ignored rest will be passed directly to the npm command
+const [ingore, ignore2, ...args] = process.argv
 
-    console.log("Starting server...");
-    const startResult = await exec("npm start");
-    console.log(startResult.stdout);
-    console.log(startResult.stderr);
-  } catch (err) {
-    console.error("Error:", err);
-  }
-}
+// windowsHide option will hide the cmd window
+execSync(`npm ${args.join(' ')}`, { windowsHide: true, stdio: 'inherit' })
+// pm2 start startscript.js --name windowsApi -- start
 
-run();
+// async function run() {
+//   try {
+//     console.log("Installing dependencies...");
+//     const installResult = await exec("npm install");
+//     console.log(installResult.stdout);
+//     console.log(installResult.stderr);
+
+//     console.log("Starting server...");
+//     const startResult = await exec("npm start");
+//     console.log(startResult.stdout);
+//     console.log(startResult.stderr);
+//   } catch (err) {
+//     console.error("Error:", err);
+//   }
+// }
+
+// run();
