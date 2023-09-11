4f6fd2b322783c5512605f4bc61433a0a3c9ce88
Andres Gaibor <androymartin99@gmail.com>
2023-09-10 21:03:44 -0500

    prueba de edicion 2

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ let errMessage = ''
 const app = Fastify({ logger: true })
 
 app.get('/', async function handler(request, reply) {
-   return { hello: 'mundo 21' }
+   return { hello: 'mundo 22' }
 })
 
 app.get('/capture', screenshotHandler)
