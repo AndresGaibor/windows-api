ce8a8c50f7a8537f34ad13b50e7fdbbe76919845
Andres Gaibor <androymartin99@gmail.com>
2023-09-11 20:50:17 -0500

    path

--- reboot_pm2.js
+++ reboot_pm2.js
@@ -1,4 +1,33 @@
-const { execSync } = require('child_process')
+const { execSync, exec, spawn } = require('child_process')
+const dotenv = require('dotenv')
+dotenv.config()
+// PM2_HOME=C:\Users\UserPC\.pm2
 // pm2 restart windowsApi
+/**
+ * [PM2][Initialization] Environment variable HOME (Linux) or HOMEPATH (Windows) are not set!
+[PM2][Initialization] Defaulting to /etc/.pm2
+unexpected error
+Error: connect EPERM //./pipe/interactor.sock
+ */
+const PM2_HOME = process.env.PM2_HOME
 
-execSync(`pm2 restart windowsApi`, { windowsHide: true, stdio: 'inherit' })
+async function reiniciar() {
+   console.log('reiniciar')
+
+   try {
+      // set HOMEPATH=C:\Users\UserPC\.pm2
+
+      const { stdout, stderr } = execSync(`set HOMEPATH=${PM2_HOME}`)
+      console.log('homepath:', stdout)
+      if (stderr) console.log('error:', stderr)
+
+      execSync(`pm2 restart windowsApi`, {
+         windowsHide: true,
+         stdio: 'inherit',
+      })
+   } catch (error) {
+      console.log(error)
+   }
+}
+
+// execSync(`pm2 restart windowsApi`, { windowsHide: true, stdio: 'inherit' })
