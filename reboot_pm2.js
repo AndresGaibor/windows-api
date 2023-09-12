3ae95bfeb30e607d654fd79326ed6b44061c9443
Andres Gaibor <androymartin99@gmail.com>
2023-09-11 20:39:22 -0500

    module to commonjs reboot pm2

--- reboot_pm2.js
+++ reboot_pm2.js
@@ -1,5 +1,4 @@
-import { execSync } from 'child_process'
-
+const { execSync } = require('child_process')
 // pm2 restart windowsApi
 
 execSync(`pm2 restart windowsApi`, { windowsHide: true, stdio: 'inherit' })
